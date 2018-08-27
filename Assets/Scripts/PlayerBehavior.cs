8587cebc2aecc7f819e51f658cf0386ffeeaeb3c
Gabriel Solis Corrêa <gabri.ell@hotmail.com>
2018-08-27 20:08:02 -0300

    Add. PlayerBehavior.cs, Prefabs e TestSprite.png

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	public enum State {Gas,Liquid,Solid}
+	public State myState;
+	public Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		if (myState == State.Gas) {
+			GoToGas ();
+		}
+		if (myState == State.Liquid) {
+			GoToLiquid ();
+		}
+		if (myState == State.Solid) {
+			GoToSolid ();
+		}
+	}
+
+	void Update () {
+	}
+
+	public void GoToGas (){
+		myState = State.Gas;
+		rb.gravityScale = -0.5f;
+		rb.mass = 0.5f;
+	}
+
+	public void GoToLiquid (){
+		myState = State.Liquid;
+		rb.gravityScale = 1;
+		rb.mass = 0.5f;
+	}
+
+	public void GoToSolid (){
+		myState == State.Solid;
+		rb.gravityScale = 1;
+		rb.mass = 1;
+	}
+
+	public void Heat(){
+		if (myState == State.Solid) {
+			GoToLiquid ();
+		}
+		if (myState == State.Liquid) {
+			GoToGas ();
+		}
+	} 
+
+	public void Cool(){
+		if (myState == State.Gas) {
+			GoToLiquid ();
+		}
+		if (myState == State.Liquid) {
+			GoToSolid ();
+		}
+	}
+}
