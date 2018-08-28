9245b9104aecb32443ff05c96e1c6c4518be08d3
Gabriel Solis Corrêa <gabri.ell@hotmail.com>
2018-08-28 13:56:03 -0300

    Add. Inputs de teste, MainScene Alterado para testes com rampas

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
 
+	//Nota: o Player está com Fixed rotation Z (Impede que o Rigidbody gire o Player ao encostar em superfícies irregulares), da pra desativar isso no Rigidbody2D
+
 	public enum State {Gas,Liquid,Solid}
 	public State myState;
 	public Rigidbody2D rb;
 
 	void Start () {
-		rb = GetComponent<Rigidbody> ();
+		rb = GetComponent<Rigidbody2D> ();
 		if (myState == State.Gas) {
 			GoToGas ();
 		}
@@ -20,26 +22,33 @@ public class PlayerBehavior : MonoBehaviour {
 			GoToSolid ();
 		}
 	}
-
-	void Update () {
+		
+	void FixedUpdate () {
+		//Inputs de teste
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+			GoToGas ();
+		if (Input.GetKeyDown (KeyCode.Alpha2))
+			GoToLiquid ();
+		if (Input.GetKeyDown (KeyCode.Alpha3))
+			GoToSolid ();
 	}
+
+	//Use os "GoTo..." para alterar imediatamente para o estado escolhido
+	//Use os "Heat" e "Cool" para alterar uma etapa na direção da temperatura escolhida (Ex: Heat altera o Solido para Liquido, e o Liquido para Gas)
 
 	public void GoToGas (){
 		myState = State.Gas;
 		rb.gravityScale = -0.5f;
-		rb.mass = 0.5f;
 	}
 
 	public void GoToLiquid (){
 		myState = State.Liquid;
 		rb.gravityScale = 1;
-		rb.mass = 0.5f;
 	}
 
 	public void GoToSolid (){
-		myState == State.Solid;
-		rb.gravityScale = 1;
-		rb.mass = 1;
+		myState = State.Solid;
+		rb.gravityScale = 3;
 	}
 
 	public void Heat(){
